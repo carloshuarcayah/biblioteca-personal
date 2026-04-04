ffde21cb90663a76e9b93195a6f31705e3a38463
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-04-04 15:29:17 -0500

    chore: configuración inicial del properties y compose para la base de datos

---
+++ src/test/java/pe/com/carlosh/bibliotecapersonal/BibliotecaPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.carlosh.bibliotecapersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaPersonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
